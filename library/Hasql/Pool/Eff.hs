09deec33908423cb3ba5b52236e86be7e4f02fe2
Michal Kleczek <michal@kleczek.org>
2023-04-07 21:26:12 +0200

    Update api. Handle QueryError properly in Eff

--- library/Hasql/Pool/Eff.hs
+++ library/Hasql/Pool/Eff.hs
@@ -3,9 +3,9 @@ module Hasql.Pool.Eff (
 ) where
 
 import Data.Functor ((<&>))
-import Effectful (Eff, IOE, (:>))
+import Effectful (Eff, IOE, raise, (:>))
 import Effectful.Dispatch.Dynamic (interpret, localSeqUnliftIO, localUnlift)
-import Effectful.Error.Static (Error, throwError)
+import Effectful.Error.Static (Error, catchError, runErrorNoCallStack, throwError)
 import Hasql.Api.Eff.WithResource
 import Hasql.Connection (Settings)
 import Hasql.Pool (Pool, UsageError, use)
@@ -14,11 +14,10 @@ import Hasql.Session
 import Prelude
 
 {-# INLINEABLE runWithPool #-}
-runWithPool :: (Error UsageError :> es, IOE :> es) => Pool -> Eff (WithConnection : es) a -> Eff es a
+runWithPool :: forall es a. (Error UsageError :> es, IOE :> es) => Pool -> Eff (WithConnection : es) a -> Eff es a
 runWithPool pool = interpret $ \env (WithResource action) -> do
     result <- localSeqUnliftIO env $ \unlift ->
-        use pool $ Session $ \connection ->
-            unlift (action connection) <&> Right
+        use pool $ Session $ \connection -> unlift $ (runErrorNoCallStack . raise . action) connection
     either throwError pure result
 
 dynamicPool :: (IOE :> es) => Int -> Maybe Int -> Settings -> Eff (DynamicResource Pool : es) a -> Eff es a
